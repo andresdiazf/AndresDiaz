ef9a156edecf3af1bb7e7f46ecb2ba695e04b1a1
AndresDiaz <andresdiazfigueroa@outlook.com>
2025-06-13 21:43:28 -0500

    actualizamos print para el tercer commit

--- holagit.py
+++ holagit.py
@@ -1,7 +1,2 @@
-print("Hola, mundo!")
-# This is a simple Python script that prints "Hola, mundo!" to the console.
-# It serves as a basic example of how to create a Python script.        
-if __name__ == "__main__":
-    print("Hola, mundo!")
-    # The main block of the script that executes when run directly.
-    # It prints "Hola, mundo!" to the console.     
+print("Hola, mundo! para el tercer commit")
+     
